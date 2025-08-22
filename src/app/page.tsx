2c428962b51de53c06ebe7e68521c2c00feb8f1a
Cristofer <cristoferespindola@outlook.com>
2025-08-21 23:33:21 -0300

    style: update form styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,7 @@ export default function Home() {
             "Last Name": formData.lastName,
             Email: formData.email,
             Phone: formData.phone,
-            "Are you bringing a plus one": formData.plusOne,
+            "Number of Guests": formData.plusOne,
             Date: new Date().toISOString().split("T")[0],
           }),
         }
@@ -91,143 +91,138 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-black text-white">
-      <main className="max-w-5xl mx-auto px-4 py-12">
+      <main className="max-w-3xl mx-auto px-4 py-12 flex flex-col gap-10 lg:gap-20">
         {/* Logo */}
-        <div className="flex justify-center mb-12">
+        <div className="flex justify-center">
           <Logo />
         </div>
 
         {/* Main Heading */}
-        <div className="text-center mb-4 lg:mb-8 max-w-md mx-auto mt-12 lg:mt-16">
+        <div className="text-center max-w-3xs mx-auto">
           <h1
-            className="text-3xl text-white mb-2 px-8"
+            className="text-3xl text-white"
             style={{
               textAlign: "center",
               fontFamily: "RH Serif",
-              fontSize: "38px",
+              fontSize: "29px",
               fontStyle: "normal",
               fontWeight: "300",
-              lineHeight: "48px",
-              letterSpacing: "-1.3px",
+              lineHeight: "32px",
               textTransform: "uppercase",
             }}
           >
             PLEASE CONFIRM YOUR RSVP
           </h1>
-          <p className="text-white text-sm font-rh-sans pt-5" style={{
-              fontSize: "16px",
-              fontStyle: "normal",
-              fontWeight: "300",
-              lineHeight: "24px",
-              letterSpacing: "0.24px",
-          }}>
-            Kindly provide the following details.
-          </p>
         </div>
 
         {/* Form */}
-        <form onSubmit={handleSubmit} className="space-y-6 flex flex-col lg:gap-4">
+        <form
+          onSubmit={handleSubmit}
+          className="space-y-6 flex flex-col gap-20"
+        >
           {/* Name Fields */}
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-4">
-            <div>
-              <input
-                type="text"
-                id="firstName"
-                name="firstName"
-                value={formData.firstName}
-                onChange={handleInputChange}
-                required
-                className="w-full px-3 py-2 h-12 bg-black border border-white text-white rounded-none focus:outline-none focus:border-white font-rh-sans font-light placeholder-gray-400"
-                placeholder="First Name"
-              />
+          <div className="flex flex-col gap-8 mb-0">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-7">
+              <div>
+                <input
+                  type="text"
+                  id="firstName"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleInputChange}
+                  required
+                  className="w-full px-3 py-2 h-12 bg-black border border-white text-white rounded-none focus:outline-none focus:border-white font-rh-sans font-light placeholder-gray-400"
+                  placeholder="First Name"
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleInputChange}
+                  required
+                  className="w-full px-3 py-2 h-12 bg-black border border-white text-white rounded-none focus:outline-none focus:border-white font-rh-sans font-light placeholder-gray-400"
+                  placeholder="Last Name"
+                />
+              </div>
             </div>
-            <div>
-              <input
-                type="text"
-                id="lastName"
-                name="lastName"
-                value={formData.lastName}
-                onChange={handleInputChange}
-                required
-                className="w-full px-3 py-2 h-12 bg-black border border-white text-white rounded-none focus:outline-none focus:border-white font-rh-sans font-light placeholder-gray-400"
-                placeholder="Last Name"
-              />
+
+            {/* Email and Phone Fields */}
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-4 mb-0">
+              <div>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  required
+                  className="w-full px-3 py-2 h-12 bg-black border border-white text-white rounded-none focus:outline-none focus:border-white font-rh-sans font-light placeholder-gray-400"
+                />
+              </div>
+              <div>
+                <input
+                  type="tel"
+                  id="phone"
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleInputChange}
+                  className="w-full px-3 py-2 h-12 bg-black border border-white text-white rounded-none focus:outline-none focus:border-white font-rh-sans font-light placeholder-gray-400"
+                  placeholder="Phone"
+                />
+              </div>
+
+              <div className="flex flex-col gap-4 relative">
+                <div className="w-full">
+                  <Select onValueChange={handleSelectChange}>
+                    <SelectTrigger className="w-full px-3 py-2 !h-12 text-md bg-black border border-white text-white rounded-none focus:outline-none focus:border-white appearance-none font-rh-sans font-light">
+                      <SelectValue
+                        className="text-white font-rh-sans text-lg"
+                        placeholder="Number of Guests"
+                      />
+                    </SelectTrigger>
+                    <SelectContent className="bg-black border border-white text-white rounded-none">
+                      <SelectItem value="1">1</SelectItem>
+                      <SelectItem value="2">2(you and a a guest)</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
             </div>
           </div>
 
-          {/* Email and Phone Fields */}
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-4 mb-0">
-            <div>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Email"
-                value={formData.email}
-                onChange={handleInputChange}
-                required
-                className="w-full px-3 py-2 h-12 bg-black border border-white text-white rounded-none focus:outline-none focus:border-white font-rh-sans font-light placeholder-gray-400"
-              />
-            </div>
-            <div>
-              <input
-                type="tel"
-                id="phone"
-                name="phone"
-                value={formData.phone}
-                onChange={handleInputChange}
-                className="w-full px-3 py-2 h-12 bg-black border border-white text-white rounded-none focus:outline-none focus:border-white font-rh-sans font-light placeholder-gray-400"
-                placeholder="Phone"
-              />
-            </div>
+          <div className="flex flex-col justify-center relative">
+            {/* Status Messages */}
+            <div className="absolute -top-16 left-0 w-full">
+              {submitStatus === "success" && (
+                <div className="p-4 bg-transparent border border-white">
+                  <p className="text-sm font-medium text-white font-rh-sans">
+                    RSVP submitted successfully!
+                  </p>
+                </div>
+              )}
 
-            <div className="flex flex-col gap-4 relative">
-              <div className="w-full absolute -top-6 left-0">
-                <p className="text-white text-sm mb-2 font-rh-sans font-light">
-                  Will you be bringing a plus one?
-                </p>
-              </div>
-              <div className="w-full">
-                <Select onValueChange={handleSelectChange}>
-                  <SelectTrigger className="w-full px-3 py-2 !h-12 text-md bg-black border border-white text-white rounded-none focus:outline-none focus:border-white appearance-none font-rh-sans font-light">
-                    <SelectValue className="text-white font-rh-sans text-lg" placeholder="Select" />
-                  </SelectTrigger>
-                  <SelectContent className="bg-black border border-white text-white rounded-none">
-                    <SelectItem value="Yes">Yes</SelectItem>
-                    <SelectItem value="No">No</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
+              {submitStatus === "error" && (
+                <div className="p-4 bg-transparent border border-red-700">
+                  <p className="text-sm font-medium text-red-300">
+                    Error submitting RSVP. Please try again.
+                  </p>
+                </div>
+              )}
             </div>
-          </div>
 
           {/* Submit Button */}
-          <div className="pt-4 lg:pt-6 flex justify-center">
             <button
               type="submit"
               disabled={isSubmitting}
               className="w-1/2 mx-auto bg-white text-black font-rh-sans font-roman py-4 px-8 hover:bg-gray-100 focus:outline-none transition duration-200 disabled:opacity-50 disabled:cursor-not-allowed"
             >
-              {isSubmitting ? "SUBMITTING..." : "SUBMIT"}
+              {isSubmitting ? "SUBMITTING..." : "CONFIRM"}
             </button>
           </div>
-
-          {/* Status Messages */}
-          {submitStatus === "success" && (
-            <div className="mt-6 p-4 bg-green-900 border border-green-700">
-              <p className="text-sm font-medium text-green-300">
-                RSVP submitted successfully!
-              </p>
-            </div>
-          )}
-
-          {submitStatus === "error" && (
-            <div className="mt-6 p-4 bg-red-900 border border-red-700">
-              <p className="text-sm font-medium text-red-300">
-                Error submitting RSVP. Please try again.
-              </p>
-            </div>
-          )}
         </form>
       </main>
     </div>
