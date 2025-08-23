4e7c44498658ae645d40bd3e676f87eff7bc6093
Cristofer <cristoferespindola@outlook.com>
2025-08-23 20:52:03 -0300

    feat: fix build

---
+++ src/app/vimeo/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function VimeoPage() {
+  return (
+    <div>
+      <iframe
+        src="https://player.vimeo.com/video/1102841980?playsinline=0"
+        width="640"
+        height="360"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
